b54fc219bdd49ba20cb45a7f98035454ec3f77d9
aniruddha <aniruddhathakur76@gmail.com>
2023-11-30 20:14:59 +0530

    Fixed Multiple Checkboxes and Image Selection Issue

--- content.js
+++ content.js
@@ -5,14 +5,11 @@
 }
 )();
 
+let imageUrls = [];
+
 function startExtension() {
 
     async function downloadSelectedImages() {
-        const selectedImages = document.querySelectorAll('input[type="checkbox"]:checked');
-        const imageUrls = Array.from(selectedImages).map(checkbox => {
-            return checkbox.parentNode.parentElement.querySelector("img").src;
-        });
-
         if (imageUrls.length > 0) {
             const zip = new JSZip();
             const folder = zip.folder("Instagram-Posts");
@@ -44,6 +41,8 @@ function startExtension() {
             } catch (error) {
                 console.error("Error creating zip file:", error);
             }
+        } else {
+            alert("Select At Least One Image!")
         }
     }
 
@@ -167,6 +166,12 @@ function startExtension() {
 
             checkbox.addEventListener("click", function (event) {
                 event.stopPropagation();
+                let imageUrlToPush = checkbox.parentNode.parentElement.querySelector("img").src;
+                if (imageUrlToPush) {
+                    console.log(imageUrlToPush);
+                    imageUrls.push(imageUrlToPush);
+                    console.log("Added that into download list!!")
+                }
             });
 
             image.addEventListener("click", function (event) {
@@ -180,10 +185,42 @@ function startExtension() {
     const mutationsInArtical = new MutationObserver(mutations => {
         mutations.forEach(record => {
             let rowAdded = record.addedNodes[0].className;
-            if (rowAdded === "_ac7v  _al3n"){
-                createCheckboxes();
+            if (rowAdded === "_ac7v  _al3n") {
+                let addedImages = record.addedNodes[0].querySelectorAll("img");
+                for (let j = 0; j < addedImages.length; j++) {
+                    let parent = addedImages[j].parentNode;
+                    var checkboxContainer = document.createElement("div");
+                    checkboxContainer.id = "checkboxContainer";
+                    checkboxContainer.style.position = "absolute";
+                    checkboxContainer.style.top = "0";
+                    checkboxContainer.style.left = "0";
+                    checkboxContainer.style.zIndex = "2";
+
+                    var checkbox = document.createElement("input");
+                    checkbox.id = "imageCheckbox";
+                    checkbox.type = "checkbox";
+                    checkbox.style.height = "20px";
+                    checkbox.style.width = "20px";
+                    checkboxContainer.appendChild(checkbox);
+
+                    parent.appendChild(checkboxContainer);
+
+                    checkbox.addEventListener("click", function (event) {
+                        event.stopPropagation();
+                        let imageUrlToPush = checkbox.parentNode.parentElement.querySelector("img").src;
+                        if (imageUrlToPush) {
+                            console.log(imageUrlToPush);
+                            imageUrls.push(imageUrlToPush);
+                            console.log("Added that into download list!!")
+                        }
+                    });
+
+                    parent.addEventListener("click", function (event) {
+                        event.stopPropagation();
+                    });
+                }
             }
-        })
+        });
     });
 
     let article = document.querySelectorAll("article")[0];
@@ -191,7 +228,7 @@ function startExtension() {
 }
 
 let headerFound = document.querySelector("h1");
-if(headerFound){
+if (headerFound) {
     startExtension();
 }
 
